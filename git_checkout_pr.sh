6b2f7614a5b10304e3f2c386ab8dadc5e160292b
Zuzana Lena Ansorgova <zuansorg@redhat.com>
2026-01-19 12:59:55 +0100

    Extend for GitLab

--- git_checkout_pr.sh
+++ git_checkout_pr.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
 if [ -z "$1" -o -z "$2" ]; then
-	echo "Script to fetch a branch from a pull request and checkout the branch."
+	echo "Fetch a branch from a PR/MR and checkout the branch."
 	echo "Usage:"
-	echo "    git-checkout-pr <PR ID> <BRANCHNAME>"
+	echo "    git-checkout-pr <PR/MR ID> <BRANCHNAME>"
 	exit 1
 else
 	ID=$1
 	BRANCHNAME=`echo $2 | sed 's/^.*://'`
 fi
 
-# Modify the remote repo name to your repo setup - this is not the standard name!
-REMOTE=upstream
+# Get the URL of the 'origin' remote
+REMOTE_URL=$(git remote get-url origin 2>/dev/null)
 
-echo "Fetching from remote: $REMOTE"
+if [ -z "$REMOTE_URL" ]; then
+    echo "Error: No remote 'origin' found."
+    exit 1
+fi
 
-git fetch $REMOTE pull/$ID/head:$BRANCHNAME
+# Check the URL for the provider name
+case "$REMOTE_URL" in
+    *github*)
+        SERVER=GitHub
+	# Modify the GitHub remote name to your setup
+	# This is not the standard name (origin)!
+	REMOTE=upstream
+	PREFIX=pull
+	;;
+    *gitlab*)
+	SERVER=GitLab
+	REMOTE=origin
+	PREFIX=merge-requests
+	;;
+    *)
+        echo "Error: Remote provider is unknown or not GitHub/GitLab."
+	exit 2
+        ;;
+esac
+
+echo "Fetching from remote: $REMOTE at $SERVER"
+git fetch $REMOTE $PREFIX/$ID/head:$BRANCHNAME
+
 git checkout $BRANCHNAME
 
 echo
-echo "I: Done."
+echo "Info: Done."
